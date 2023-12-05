40b98676657c007de977e7dc903b86d30424dcd3
Gene Hern <103822848+genehern@users.noreply.github.com>
2023-12-05 16:48:25 +0800

    Update csv_analyser.py

--- csv_analyser.py
+++ csv_analyser.py
@@ -1,21 +1,21 @@
 import csv
 
-number_of_choices = int(input("Number of choices given to students (Give a number): "))
+number_of_choices = 6 #UPDATE ME
 counterdict = {} #dictionary to count number of students in each course
 for i in range (number_of_choices):
     counterdict[i] = 0
     
-word_to_number = {'1st Choice': 0, '2nd Choice' : 1, '3rd Choice': 2, '4th Choice' : 3, '5th Choice' : 4, '6th Choice' : 5}
-assign_class_tonum = { '4B' : 0 , '4C' : 1, '4I' : 2, "4K" : 3, '4M' : 4, '4P' : 5, '4R' : 6}
+word_to_number = {'1st Choice': 0, '2nd Choice' : 1, '3rd Choice': 2, '4th Choice' : 3, '5th Choice' : 4, '6th Choice' : 5} #UPDATE ME
+assign_class_tonum = { '4B' : 0 , '4C' : 1, '4I' : 2, "4K" : 3, '4M' : 4, '4P' : 5, '4R' : 6} #UPDATE ME
 
 stdl = [] #list of students
 
 class student:
     #c1 is colum 1, c2 is column 2, etc
-    def __init__(self, name, clas, c1, c2, c3, c4, c5, c6):
+    def __init__(self, name, clas, c1, c2, c3, c4, c5, c6): #UPDATE ME
         self.name = name.upper()
         self.clas = clas
-        self.choice = [c1, c2, c3, c4, c5, c6]
+        self.choice = [c1, c2, c3, c4, c5, c6] #UPDATE ME
         self.course = int()
 
     def sort(self):
@@ -28,7 +28,7 @@ class student:
                             return
 
 duplicate = dict() 
-with open ('data1.csv') as csvfile:
+with open ('data.csv') as csvfile:
     csv_reader = csv.reader(csvfile)
     next(csv_reader)
     #saving as a dictionary first to remove all duplicate responses via checking email (Some students send in multiple responses with same email)
@@ -37,7 +37,7 @@ with open ('data1.csv') as csvfile:
         
 objl = list(duplicate.values())
 for row in objl:    
-    stdl.append(student(row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]))
+    stdl.append(student(row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9])) #UPDATE ME
 
 for students in stdl:
     students.sort()
